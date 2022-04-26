64003bc80694e61303df0e446dfeeb964ad97e01
cryptodev26 <cryptodev26@gmail.com>
2022-04-26 21:33:47 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button,  Card, Row, Form, Tab, Col, Nav, ButtonGroup, Navbar, Container} from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{  backgroundImage: `url(./background.png)`}}>
+        <img src="./logo.png" alt="Italian Trulli"/>
+      </div>
+    );
+  }
+}
+
+export default App;
